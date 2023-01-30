a04fccd26d2abd2aeed10dfad6671ae1b2ad81cc
mohamed elkhayat <elkhayat705@gmail.com>
2023-01-30 07:12:38 -0500

    fetch  info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React ,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from "./components/Layout/Layout";
 import Home from './components/Home/Home';
 import About from './components/About/About';
@@ -11,39 +11,38 @@ import axios from 'axios';
 
 function App() {
 
-     
-  //creating function to load ip address from the API
-  const getData = async()=>{
-     axios.get('https://geolocation-db.com/json/')
-     .then(res=>{
-      console.log(res.data);
-      NotifyVisite(res.data)
-      console.log("with data");
-    })
-    .catch(res=>{
-      const obj = {
-        ip: "uncnkonwn"
-      }
-      NotifyVisite(obj)
-      console.log("with data");
-    })
-     
-}
 
-useEffect( ()=>{     
+  //creating function to load ip address from the API
+  const getData = async () => {
+    axios.get('https://geolocation-db.com/json/')
+      .then(res => {
+        NotifyVisite(res.data)
+        console.log("welcome ...");
+      })
+      .catch(res => {
+        const obj = {
+          ip: "uncnkonwn"
+        }
+        NotifyVisite(obj)
+        console.log("welcome  ... X");
+      })
+
+  }
+
+  useEffect(() => {
     getData();
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Routes>
-          <Route path="/" element={<Layout/>} >
-          <Route index element={<Home/>} />
-          <Route path='about' element={<About/>} />
-          <Route path='contact' element={<Contact/>} />
-          <Route path='projects' element={<Projects/>} />
-        </Route>
-        
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='projects' element={<Projects />} />
+      </Route>
+
     </Routes>
   );
 }
